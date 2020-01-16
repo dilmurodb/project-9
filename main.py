5fd386259daf01e55f12e48aa00bda58625549d8
bukharov <dilmurod.bukharov@gmail.com>
2020-01-16 17:03:45 -0500

    installed dependencies and created a py file also made a model and created database

---
+++ main.py
@@ -0,0 +1,27 @@
+from peewee import *
+from datetime import date
+
+db = PostgresqlDatabase('contacts', user='postgres', password='',
+                        host='localhost', port=5432)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Contact(BaseModel):
+    name = CharField()
+    birthday = DateField()
+    phone = CharField()
+    email = CharField()
+    address = CharField()
+
+db.connect()
+
+#remove this when you want your databvase to start saving forever
+db.drop_tables([Contact])
+
+db.create_tables([Contact])
+
+dilmurod = Contact(name='Dilmurod Bukharov', birthday=date(1989, 11, 17), phone='4121234567', 
+email='d.bukharov@gmail.com', address='Washington DC')
+dilmurod.save()
