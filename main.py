940b076eb909b38f14bfd110bc538b420fc44d5e
bukharov <dilmurod.bukharov@gmail.com>
2020-01-19 19:37:56 -0500

    added one more functionality, findinga a contact based on the first name

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from peewee import *
-# from datetime import date
 
 db = PostgresqlDatabase('contacts', user='postgres', password='',
                         host='localhost', port=5432)
 
-intro_question = input("What would you like to do with Contacts? Create? Read? Delete? ")
-# contact_info = input("Please enter your contact info:")
+intro_question = input("What would you like to do with Contacts? Create? Read? Find? Delete? ")
 
 
 def read_contact():
@@ -13,11 +11,7 @@ def read_contact():
     for contact in contacts:
         print(contact)
         print(contact.firstname + " " + contact.lastname + " " + contact.phone + " " + contact.email + " " + contact.address)
-        # print(contact.name)
 
-# def update_contact(update_contact):
-#     contacts = Contact.get(Contact.name == update_contact)
-#     Contact.phone =
 
 def create_contact():
     contact_firstname = input("Enter First Name: ")
@@ -29,8 +23,13 @@ def create_contact():
     newcontact.save()
     print(newcontact.firstname + " " + newcontact.lastname + " " + newcontact.phone + " " + newcontact.email + " " + newcontact.address)
 
+def find_contact():
+    find_contact_by_firstname = input("Enter First Name of the contact you want to find: ")
+    find_by_firstname = Contact.get(Contact.firstname == find_contact_by_firstname)
+    print(find_by_firstname.firstname + " " + find_by_firstname.lastname + " " + find_by_firstname.phone + " " + find_by_firstname.email + " " + find_by_firstname.address)
+
 def delete_contact():
-    contact_name_delete = input("Enter The first name of the contact you want to delete: ")
+    contact_name_delete = input("Enter First Name of the contact you want to delete: ")
     contact_firstname = Contact.get(Contact.firstname == contact_name_delete)
     contact_firstname.delete_instance()
 
@@ -44,39 +43,20 @@ class Contact(BaseModel):
     phone = CharField()
     email = CharField()
     address = CharField()
-# read_contact()
+
 db.connect()
 db.create_tables([Contact])
 
 if intro_question == "Create":
-    # contact_input = input("Enter contact First Name: ")
     create_contact()
 
 
 elif intro_question == "Read":
-    # update_contact = input("Enter First Name: ")
     read_contact()
-
-    # update_contact = input("Enter Phone Number: ")
-    # update_contact = input("Enter Email: ")
 
 
 elif intro_question == "Delete":
-    # delete_contact = input("Enter the Full Name of the contact you want to delete: ")
     delete_contact()
 
-
-
-#remove this when you want your database to start saving forever
-# db.drop_tables([Contact])
-
-# db.create_tables([Contact])
-
-# dilmurod = Contact(firstname='Dilmurod', lastname='Bukharov', phone='4121234567', 
-# email='d.bukharov@gmail.com', address='Washington DC')
-# someperson = Contact(firstname='Some', lastname='Person', phone='9876543251', 
-# email='d.bukharov@ganymail.com', address='NYC')
-# dilmurod.save()
-# someperson.save()
-
-# read_contact()
+elif intro_question == "Find":
+    find_contact()
